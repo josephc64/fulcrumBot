a9408b53a83a3f7f2624ebe1811ee4bf6fc4663f
joseph <josephkamphuis@gmail.com>
2024-08-18 22:28:52 -0500

    updates

--- bot.py
+++ bot.py
@@ -2,11 +2,13 @@
 Original Repository (Version: 6.1.0) Copyright © Krypton 2019-2023 - https://github.com/kkrypt0nn (https://krypton.ninja)
 """
 
-import json, logging, os, platform, random, sys, aiosqlite, discord, csv, re, datetime
+import json, logging, os, platform, random, sys, aiosqlite, discord, csv, re, datetime, time, asyncio
+from datetime import datetime, timedelta
 from plexapi.server import PlexServer
 from discord.ext import commands, tasks
 from discord.ext.commands import Context
 from dotenv import load_dotenv
+from playwright.async_api import async_playwright, Playwright
 
 from database import DatabaseManager
 
@@ -19,12 +21,7 @@ else:
 
 intents = discord.Intents.default()
 
-"""
-Uncomment this if you want to use prefix (normal) commands.
-It is recommended to use slash commands and therefore not use prefix commands.
-
-If you want to use prefix commands, make sure to also enable the intent below in the Discord developer portal.
-"""
+# This enables traditional prefix commands to work
 intents.message_content = True
 
 # Setup both of the loggers
@@ -84,18 +81,19 @@ class DiscordBot(commands.Bot):
             intents=intents,
             help_command=None,
         )
-        """
-        This creates custom bot variables so that we can access these variables in cogs more easily.
+        
+        # This creates custom bot variables so that we can access these variables in cogs more easily.
+        # For example, The config is available using the following code:
+            # self.config # In this class
+            # bot.config # In this file
+            # self.bot.config # In cogs
 
-        For example, The config is available using the following code:
-        - self.config # In this class
-        - bot.config # In this file
-        - self.bot.config # In cogs
-        """
         self.logger = logger
         self.config = config
         self.database = None
 
+
+    # Initialize the database
     async def init_db(self) -> None:
         async with aiosqlite.connect(
             f"{os.path.realpath(os.path.dirname(__file__))}/database/database.db"
@@ -106,10 +104,9 @@ class DiscordBot(commands.Bot):
                 await db.executescript(file.read())
             await db.commit()
 
+
+    # This code is run during bot startup, and loads all cogs
     async def load_cogs(self) -> None:
-        """
-        The code in this function is executed whenever the bot will start.
-        """
         for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
             if file.endswith(".py"):
                 extension = file[:-3]
@@ -122,145 +119,236 @@ class DiscordBot(commands.Bot):
                         f"Failed to load extension {extension}\n{exception}"
                     )
 
+    
+    # Keep game library threads active
+    @tasks.loop(minutes=1.0)
+    async def games_active(self) -> None:
+        print("Sorting task activated. 120 seconds to sort.")
+        await asyncio.sleep(120)
+        games = (bot.get_channel(1229215621875630151)).threads
+
+        gameDict = {}
+        for t in games:
+            if t.name != "Game Library Index":
+                gameDict[t.name] = t.id
+        sortDict = {key: gameDict[key] for key in sorted(gameDict)}
+        
+        idListOrdered = list(sortDict.values())
+        revListOrdered = reversed(idListOrdered)
+
+        idListOrig = list(gameDict.values())
+        
+        with open('gameDate.txt', 'r') as dateFile:
+            logDate = datetime.strptime(dateFile.read().strip(), "%Y-%m-%d")
+
+        nowDate = datetime.now().strftime("%Y-%m-%d")
+        dateDiff = datetime.strptime(nowDate, "%Y-%m-%d") - logDate
+
+        if(gameDict != sortDict) or (dateDiff.days >=2):
+            print("Sort condition met.")
+            await asyncio.sleep(28)
+            for id in revListOrdered:
+                print(f"Sorting {id}")
+                await asyncio.sleep(28)
+                message = await (bot.get_channel(id)).send("Just checking to see if this thread is still active!")
+                await asyncio.sleep(28)
+                await message.delete()
+                await (bot.get_channel(id)).edit(archived=False, auto_archive_duration=10080)
+        
+                newTime = (datetime.strptime(nowDate, "%Y-%m-%d") + timedelta(days=1)).strftime("%Y-%m-%d")
+        
+                with open('gameDate.txt', 'w') as dateFile:
+                    dateFile.write(newTime)
+        else:
+            print("Sort condition not met.")
+
+
+    # Keep game library threads active
+    @tasks.loop(minutes=1.0)
+    async def vid_stream(self) -> None:
+
+        with open('schedule.csv', newline='') as schedFile:
+            sched = csv.reader(schedFile, delimiter=' ', quotechar='|')
+            nowDate = datetime.now()
+            for row in sched:
+                itemTime = (str(row[0]).split(",")[0]) + " " + (str(row[0]).split(",")[1])
+                itemTime = datetime.strptime(itemTime, "%Y-%m-%d %H:%M:%S")
+                itemCode = str(row[0]).split(",")[2]
+
+                if(nowDate >= itemTime):
+                    print("It's time!")
+                    async with async_playwright() as p:
+                        browser = await p.chromium.launch(headless=False)
+                        page = await browser.new_page()
+                        await page.goto("http://www.youtube.com")
+                        print(await page.title())
+                        await page.pause()
+                else:
+                    print("It's not time yet!")
+
+
+    # Check the date of the last Plex log, sending another one if enough time has passed.
     @tasks.loop(minutes=1.0)
     async def plex_log(self) -> None:
-        """
-        Checks the date of the last Plex log, and makes another one if appropriate.
-        """
         bigString = ""
 
         def format_numbers(arr):
             if not arr: return ""
+            
             result = []
             start = arr[0]
             end = arr[0]
+
             for num in arr[1:]:
-                if num == end + 1: end = num
+                if num == end + 1:
+                    end = num
                 else:
-                    if start == end: result.append(f"{start:02d}")
-                    else: result.append(f"{start:02d}-{end:02d}")
+                    if start == end:
+                        result.append(f"{start:02d}")
+                    else:
+                        result.append(f"{start:02d}-{end:02d}")
                     start = num
                     end = num
-            if start == end: result.append(f"{start:02d}")
-            else: result.append(f"{start:02d}-{end:02d}")
+            if start == end:
+                result.append(f"{start:02d}")
+            else:
+                result.append(f"{start:02d}-{end:02d}")
             return ", ".join(result)
 
-        dateFile = open('plexDate.txt', 'r')
-        logDate = datetime.datetime.strptime(dateFile.read(), "%Y-%m-%d")
-        dateFile.close()
+        # Read the last log date
+        with open('plexDate.txt', 'r') as dateFile:
+            logDate = datetime.strptime(dateFile.read().strip(), "%Y-%m-%d")
 
-        nowDate = datetime.datetime.now().strftime("%Y-%m-%d")
-        dateDiff = datetime.datetime.strptime(nowDate, "%Y-%m-%d") - logDate
+        nowDate = datetime.now().strftime("%Y-%m-%d")
+        dateDiff = datetime.strptime(nowDate, "%Y-%m-%d") - logDate
 
-
-        if(dateDiff.days >= 7):
+        if dateDiff.days > 6:
             baseurl = os.getenv("PLEX_URL")
             token = os.getenv("PLEX_TOKEN")
             plex = PlexServer(baseurl, token)
             movies = plex.library.section('Movies')
             series = plex.library.section('Series')
-            allItems = [*movies.search(), *series.search()]
 
             bigString += "```\n"
-            bigString += f"FULCRUM Automated Changelog // {logDate.strftime('%Y/%m/%d')} - {datetime.datetime.now().strftime('%Y/%m/%d')}\n\n"
+            bigString += f"FULCRUM Automated Changelog // {logDate.strftime('%Y/%m/%d')} - {datetime.now().strftime('%Y/%m/%d')}\n\n"
             bigString += "Movies\n------\n"
             for m in movies.search():
-                if (m.addedAt > logDate):
-                    if (m.editionTitle == None):
+                if m.addedAt >= logDate:
+                    if m.editionTitle is None:
                         bigString += f" + {m.title} ({m.year})\n"
                     else:
                         bigString += f" + {m.title} ({m.year}) - {m.editionTitle}\n"
 
             bigString += "\nShows\n-----\n"
             for s in series.search():
-                if (s.addedAt > logDate):
-                    showEpisodes = {}
-                    for e in series.get(s.title).episodes():
-                        for part in e.iterParts():
-                            regexPattern = r"[sS](\d{1,4})[eE](\d{1,4})"
-                            epTitle = re.search(regexPattern, part.file, re.IGNORECASE)
-                            if epTitle:
-                                season = epTitle.group(1)
-                                episode = epTitle.group(2)
+                showEpisodes = {}
+                for e in s.episodes():
+                    if e.addedAt >= logDate:
+                        regexPattern = r"[sS](\d{1,4})[eE](\d{1,4})"
+                        epTitle = re.search(regexPattern, e.locations[0], re.IGNORECASE)
+                        if epTitle:
+                            season = epTitle.group(1)
+                            episode = epTitle.group(2)
+                            
+                            if season not in showEpisodes:
+                                showEpisodes[season] = []
+                            
+                            if int(episode) not in showEpisodes[season]:
+                                showEpisodes[season].append(int(episode))
                                 
-                                if season not in showEpisodes:
-                                    showEpisodes[season] = []
-                                
-                                if int(episode) not in showEpisodes[season]:
-                                    showEpisodes[season].append(int(episode))
-                                    
+                if showEpisodes:
                     bigString += f" o {s.title} ({s.year})\n"
-                    for season in showEpisodes:
-                        bigString += f"    + S{season}E{format_numbers(showEpisodes[season])}\n"
+                    for season in sorted(showEpisodes.keys(), key=int):
+                        bigString += f"    + S{season}E{format_numbers(sorted(showEpisodes[season]))}\n"
+
+            bigString += "\nGames\n-----\n"
+            gameCh = bot.get_channel(1229215621875630151)
+            games = gameCh.threads
+            gSort = []
+
+            for g in games:
+                if (((g.created_at).strftime('%Y/%m/%d') > logDate.strftime('%Y/%m/%d')) and (g.name != "Game Library Index")):
+                    gSort.append(g.name)
+                    gSort.sort()  
+            for g2 in gSort: bigString += f" + {g2}\n"
+
             bigString += "```"
 
-            dateFile = open('plexDate.txt', 'w')
-            dateFile.write(nowDate)
-            dateFile.close()
+            newTime = (datetime.strptime(nowDate, "%Y-%m-%d") + timedelta(days=1)).strftime("%Y-%m-%d")
+
+
+            with open('plexDate.txt', 'w') as dateFile:
+                dateFile.write(newTime)
 
             channel = bot.get_channel(1223505800706789378)
             await channel.send(bigString)
+            #print(bigString)
 
-
-    @plex_log.before_loop
-    async def before_plex_log(self) -> None:
-        """
-        Before starting the status changing task, we make sure the bot is ready
-        """
-        await self.wait_until_ready()
-
+    # Task to change bot status every minute
     @tasks.loop(minutes=1.0)
     async def status_task(self) -> None:
-        """
-        Setup the game status task of the bot.
-        """
-        statuses = ["watching Plex", "gaming", "shmillin"]
+        statuses = ["Plex", "Team Fortress 2", "games", "Minecraft", "Mario Kart",
+                    "Super Smash Bros.", "your mom", "Beeny", "Super Nintendo",
+                    "Dreamcast", "GameCube", "Wii U", "PS1 games", "PS2 games",
+                    "Toonami", "with dogs", "with Banjo", "the market", "chance",
+                    "with rats", "with illegal fireworks"]
         await self.change_presence(activity=discord.Game(random.choice(statuses)))
 
-    @status_task.before_loop
-    async def before_status_task(self) -> None:
-        """
-        Before starting the status changing task, we make sure the bot is ready
-        """
+    ### Necessary pre-checks to prevent loops from beginning before bot is ready ###
+
+    # Pre-check for Plex logger
+    @plex_log.before_loop
+    async def before_plex_log(self) -> None:
         await self.wait_until_ready()
 
+    # Pre-check for status loop
+    @status_task.before_loop
+    async def before_status_task(self) -> None:
+        await self.wait_until_ready()
+
+    # Pre-check for keeping threads active
+    @games_active.before_loop
+    async def before_games_active(self) -> None:
+        await self.wait_until_ready()
+
+    # Pre-check for video stream scheduling
+    #@vid_stream.before_loop
+    #async def before_vid_stream(self) -> None:
+    #    await self.wait_until_ready()
+
+
+    ### Startup sequence that runs whenever the bot boots ###
     async def setup_hook(self) -> None:
-        """
-        This will just be executed when the bot starts the first time.
-        """
         self.logger.info(f"Logged in as {self.user.name}")
         self.logger.info(f"discord.py API version: {discord.__version__}")
         self.logger.info(f"Python version: {platform.python_version()}")
-        self.logger.info(
-            f"Running on: {platform.system()} {platform.release()} ({os.name})"
-        )
+        self.logger.info(f"Running on: {platform.system()} {platform.release()} ({os.name})")
         self.logger.info("-------------------")
+
+        # Initialize databse and cogs
         await self.init_db()
         await self.load_cogs()
+
+        # Initialize task loops
         self.status_task.start()
         self.plex_log.start()
+        self.games_active.start()
+        #self.vid_stream.start()
+
+        # Start the database
         self.database = DatabaseManager(
             connection=await aiosqlite.connect(
                 f"{os.path.realpath(os.path.dirname(__file__))}/database/database.db"
             )
         )
 
+    # This code is run any time someone sends any message
     async def on_message(self, message: discord.Message) -> None:
-        """
-        The code in this event is executed every time someone sends a message, with or without the prefix
-
-        :param message: The message that was sent.
-        """
-        if message.author == self.user or message.author.bot:
-            return
+        if message.author == self.user or message.author.bot: return
         await self.process_commands(message)
 
+    # This code is run every time a command is successfully executed
     async def on_command_completion(self, context: Context) -> None:
-        """
-        The code in this event is executed every time a normal command has been *successfully* executed.
-
-        :param context: The context of the command that has been executed.
-        """
         full_command_name = context.command.qualified_name
         split = full_command_name.split(" ")
         executed_command = str(split[0])
@@ -273,13 +361,9 @@ class DiscordBot(commands.Bot):
                 f"Executed {executed_command} command by {context.author} (ID: {context.author.id}) in DMs"
             )
 
-    async def on_command_error(self, context: Context, error) -> None:
-        """
-        The code in this event is executed every time a normal valid command catches an error.
 
-        :param context: The context of the normal command that failed executing.
-        :param error: The error that has been faced.
-        """
+    # This code is run every time a command errors out
+    async def on_command_error(self, context: Context, error) -> None:
         if isinstance(error, commands.CommandOnCooldown):
             minutes, seconds = divmod(error.retry_after, 60)
             hours, minutes = divmod(minutes, 60)
@@ -330,7 +414,7 @@ class DiscordBot(commands.Bot):
             raise error
 
 
+# Load dotenv values and run the bot
 load_dotenv()
-
 bot = DiscordBot()
 bot.run(os.getenv("TOKEN"))
